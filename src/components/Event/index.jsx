4e1bc268c2b1bdd3d5bc7833fc5922b6d4d636aa
burner0621 <cutetiger0621@gmail.com>
2023-09-16 05:52:22 +0900

    border color fixed

--- src/components/Event/index.jsx
+++ src/components/Event/index.jsx
@@ -1,6 +1,6 @@
 const Event = () => {
     return (
-        <div className="p-5 w-full bg-pink-1 rounded-[10px]">
+        <div className="p-5 w-full bg-pink-1 rounded-[10px] border border-[1px] border-grey-2">
             <div className="text-black-2 text-base font-black leading-6">Flemington 5</div>
             <div className="text-black-1 text-base font-medium leading-6">Pool: $10,500</div>
             <div className="text-black-1 text-base font-medium leading-6">Market: 101.5%</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import plugin from 'tailwindcss/plugin'
+
 export default {
   content: [
     "./index.html",
@@ -7,6 +9,7 @@ export default {
   theme: {
     extend: {
       backgroundColor: {
+        'grey-2': '#EAECF0',
         "pink-1": '#F2F4F7',
         "pink-2": "#FEE4E2",
         "green-2": "#D1FADF",
@@ -17,17 +20,37 @@ export default {
         "blue-1": "#2E90FA",
         "grey-1": "#98A2B3",
         "grey-2": "#667085",
-        "green-1": "#039855",
+        "green-1": "#054F31",
+        "pink-2": "#FEE4E2",
         "red-1": "#D92D20",
+        "red-2": "#7A271A"
       },
       borderColor: {
-        'grey-1': '#D0D5DD'
+        'grey-2': '#EAECF0',
+        'grey-1': '#D0D5DD',
       },
       gridTemplateColumns: {
         '14': 'repeat(14, minmax(0, 1fr))',
       }
     },
+    textShadow: {
+      sm: '0px 0px 4px var(--tw-shadow-color)',
+      DEFAULT: '2px 2px 4px var(--tw-shadow-color)',
+      lg: '4px 4px 8px var(--tw-shadow-color)',
+      xl: '4px 4px 16px var(--tw-shadow-color)',
+    }
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    })
+  ],
 }
 
